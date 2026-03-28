187f977ff3841a5ea3524ab52434683f739334ad
qingqing-Na <narenbnu@gmail.com>
2026-03-28 00:14:36 -0400

    Update test_search.py

--- test/test_search.py
+++ test/test_search.py
@@ -1,21 +1,53 @@
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
+import pytest
+from selenium import webdriver
+from pages.login_page import LoginPage
 
 
-class InventoryPage(BasePage):
 
-    SEARCH_BOX = (By.CLASS_NAME, "product_sort_container")  
-    PRODUCT_NAMES = (By.CLASS_NAME, "inventory_item_name")
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.lcell.bnu.edu.cn/")
+    yield driver
+    driver.quit()
 
-    def search_product(self, keyword):
-       
-        self.keyword = keyword.lower()
 
-    def get_product_names(self):
-        elements = self.driver.find_elements(*self.PRODUCT_NAMES)
-        names = [el.text for el in elements]
+@pytest.fixture
+def logged_in_driver(driver):
+    login = LoginPage(driver)
+    login.login("standard_user", "secret_sauce")
+    return driver
 
-        if hasattr(self, "keyword") and self.keyword:
-            return [n for n in names if self.keyword in n.lower()]
 
-        return names
+# ✅ Test 1: Search (Filter) a specific class
+def test_search_specific_class(logged_in_driver):
+    classlist = classPage(logged_in_driver)
+
+    classlist.search_class("English")
+
+    results = classlist.get_class_names()
+
+    assert any("English" in item for item in results)
+
+
+# ✅ Test 2: Search with no matching result
+def test_search_no_result(logged_in_driver):
+    classlist = classPage(logged_in_driver)
+
+    classlist.search_class("No Results")
+
+    results = classlist.get_class_names()
+
+    assert len(results) == 0
+
+
+# ✅ Test 3: Empty search (should show all class)
+def test_search_empty(logged_in_driver):
+    classlist = classPage(logged_in_driver)
+
+    classlist.search_class("")
+
+    results = classlist.get_class_names()
+
+    assert len(results) > 0
